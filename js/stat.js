a21fc03ede96f743b1a121a880144227222cffad
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-26 03:43:59 +0600

    fix criteries

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,27 @@
 
 'use strict';
 
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var INDENT = BAR_WIDTH + 50;
+var INITIAL_X = 120;
+var INITIAL_Y = 90;
+var VERTICAL_INDENT = 15;
+var HISTOGRAM_HEIGHT = 150;
+
+var RESULTS_TEXT_INITIAL_X = INITIAL_X;
+var RESULTS_TEXT_INITIAL_Y = 80;
+var RESULTS_WINDOW_BACKGROUND_COLOR = 'rgba(255, 255, 255, 1.0)';
+
+var DEFAULT_USER_NAME_COLOR = 'rgba(255, 0, 0, 1)';
+var BLACK_COLOR = 'rgba(0, 0, 0, 1.0)';
+
+var SHADOW_OFFSET = 10;
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var SHADOW_FONT = '14px PT Mono';
+
+var TITLE_TEXT = 'Ура вы победили!\nСписок результатов:';
+
 /**
  * Вызывается каждый раз когда игрок проходит уровень
  *
@@ -10,49 +31,43 @@
  */
 window.renderStatistics = function (ctx, names, times) {
 
-  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.fillStyle = RESULTS_WINDOW_BACKGROUND_COLOR;
   ctx.strokeRect(100, 10, 420, 270);
 
   // устанавливаем параметры для тени
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
+  ctx.shadowColor = SHADOW_COLOR;
+  ctx.shadowOffsetX = SHADOW_OFFSET;
+  ctx.shadowOffsetY = SHADOW_OFFSET;
   ctx.fillRect(100, 10, 420, 270);
 
-  ctx.fillStyle = '#000';
-  ctx.font = '14px PT Mono';
+  ctx.fillStyle = BLACK_COLOR;
+  ctx.font = SHADOW_FONT;
 
   // не даём применять тень ко всему что есть в прямоугольнике
   ctx.shadowColor = 'transparent';
 
-  var headerText = 'Ура вы победили!\nСписок результатов:';
+  var headerText = TITLE_TEXT;
   var array = headerText.split('\n');
   var height = 20;
 
   for (var i = 0; i < array.length; i++) {
     height += 20;
-    ctx.fillText(array[i], 120, height);
+    ctx.fillText(array[i], INITIAL_X, height);
   }
 
-  var histogramHeight = 150;
   var maxIndex = getMaxIndex(times);
   var maxTime = times[maxIndex];
-  var step = histogramHeight / (maxTime - 0);
+  var step = HISTOGRAM_HEIGHT / (maxTime - 0);
 
-  ctx.fillText('Худшее вермя: ' + maxTime.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 80);
-
-  var barHeight = 150;
-  var barWidth = 40;
-  var indent = barWidth + 50;
-  var initialX = 120;
-  var initialY = 90;
-  var verticalIndent = 15;
+  ctx.fillText('Худшее вермя: ' + maxTime.toFixed(2) + 'мс у игрока ' + names[maxIndex], RESULTS_TEXT_INITIAL_X, RESULTS_TEXT_INITIAL_Y);
 
   for (var i = 0; i < times.length; i++) {
+    // рисуем гистограмму
     ctx.fillStyle = getBarRandomColorOrDefault(names[i]);
-    ctx.fillRect(initialX + indent * i, initialY + (barHeight - times[i] * step), barWidth, times[i] * step);
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(names[i], initialX + indent * i, initialY + barHeight + verticalIndent);
+    ctx.fillRect(INITIAL_X + INDENT * i, INITIAL_Y + (BAR_HEIGHT - times[i] * step), BAR_WIDTH, times[i] * step);
+    // сбрасываем цвет и рисуем текст
+    ctx.fillStyle = BLACK_COLOR;
+    ctx.fillText(names[i], INITIAL_X + INDENT * i, INITIAL_Y + BAR_HEIGHT + VERTICAL_INDENT);
   }
 }
 
@@ -83,11 +98,11 @@ var getMaxIndex = function (times) {
  */
 function getBarRandomColorOrDefault(name) {
 
-  return name === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 77, 255, 0.' + getRandomIntInclusive(1, 10) + ')';
+  return name === 'Вы' ? DEFAULT_USER_NAME_COLOR : 'rgba(0, 77, 255, 0.' + getRandomIntInclusive(1, 10) + ')';
 }
 
 /**
- * Вовзращает случайное число в указанном интервале
+ * Возвращает случайное число в указанном интервале
  * @param {Number} min минимальное число
  * @param {Number} max максимальное числа
  * @return {Number} случайное значение
